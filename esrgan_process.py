46038884892db48e983e78a6ad9f96baac901f1c
Ali Can Gönüllü <alicangonullu@yahoo.com>
2023-11-01 23:38:13 +0300

    Image Upscaling with AI
    Image Upscaling with AI

--- esrgan_process.py
+++ esrgan_process.py
@@ -2,13 +2,15 @@ import torch
 from PIL import Image
 from RealESRGAN import *
 
-input = 'demo.png'
-output = 'output.png'
+input_pic = 'pic\\demo.png'
+output_pic = 'pic\\output.png'
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = RealESRGAN(device)
+model = RealESRGAN(device, scale=4, anime=False)
+# If anime is true : model.load_weights('model/RealESRGAN_x4plus_anime_6B.pth')
 model.load_weights('model/RealESRGAN_x4plus.pth')
-image = Image.open(input).convert('RGB')
+image = Image.open(input_pic).convert('RGB')
 sr_image = model.predict(image)
-sr_image.save(output)
-DifferencesCalculate.display(input, output)
+sr_image.save(output_pic)
+DifferencesCalculate.display(input_pic, output_pic)
+RealESRGAN.face_enchange(output_pic)
