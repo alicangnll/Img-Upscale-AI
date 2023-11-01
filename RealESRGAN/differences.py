3c4aaaeee4a9b1192a24c5804b44329bb3cfeff5
Ali Can Gönüllü <alicangonullu@yahoo.com>
2023-11-01 21:39:01 +0300

    Image Upscaling with AI

--- RealESRGAN/differences.py
+++ RealESRGAN/differences.py
@@ -3,10 +3,10 @@ import os
 import matplotlib.pyplot as plt
 
 class DifferencesCalculate:
-   def display(input_folder, result_folder):
+  def display(input_folder, result_folder):
     dir = os.getcwd()
-    img_input = imread(dir + "\\" + input_folder)
-    img_output = imread(dir + "\\" + result_folder)
+    img_input = DifferencesCalculate.imread(dir + "\\" + input_folder)
+    img_output = DifferencesCalculate.imread(dir + "\\" + result_folder)
     fig = plt.figure(figsize=(25, 10))
     ax1 = fig.add_subplot(1, 2, 1) 
     plt.title('Original Image (In)', fontsize=16)
@@ -16,7 +16,8 @@ class DifferencesCalculate:
     ax2.axis('off')
     ax1.imshow(img_input)
     ax2.imshow(img_output)
-def imread(img_path):
-  img = cv2.imread(img_path)
-  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  return img
+  
+  def imread(img_path):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
